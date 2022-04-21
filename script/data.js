c5e72c68565c05bc5aef63dc0462b1e18011bf86
Baibhav Mandal <baibhavmandal1@gmail.com>
2022-04-21 10:00:11 +0530

    .

---
+++ script/data.js
@@ -0,0 +1,16 @@
+const type = ["symmetric", "asymmetric"];
+const symmetric = 
+[
+    {
+        id: 1,
+        name: "shiftCipher"
+    },
+    {
+        id: 2,
+        name: "xorCipher"
+    }
+    {
+        id: 3,
+        name: "rotateCipher"
+    }
+]
--- script/xorCipher.js
+++ script/xorCipher.js
@@ -3,21 +3,13 @@ const plainText = document.getElementById("plainText");
 const key = document.getElementById("key");
 const cipherText = document.getElementsByClassName("cipherText");
 
-function rotateCipher(input, k) {
-  var result = "";
-  var arr = Array.from(input);
-  var store = "";
-  for (let i = 0; i < k; i++) {
-    var temp = arr[arr.length - 1];
-    for (let j = 0; j < arr.length; j++) {
-      store = arr[j];
-      arr[j] = temp;
-      temp = store;
-    }
-    console.log(arr);
+function xorCipher(input, k) {
+  let result = "";
+  for (let index = 0; index < input.length; index++) {
+    result += String.fromCharCode(
+      input.charCodeAt(index) ^ k.charCodeAt(index % k.length)
+    );
   }
-  result = arr;
-  console.log(typeof arr);
   cipherText[0].innerHTML = "<h3>" + result + "</h3>";
 }
 
@@ -26,5 +18,5 @@ submit.addEventListener("click", (e) => {
 
   var input = plainText.value;
   var k = key.value;
-  rotateCipher(input, k);
+  xorCipher(input, k);
 });
